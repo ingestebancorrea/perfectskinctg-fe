c82adc03a051d69b968f3e13d5cbaa1d8bbb229b
Ing. Omar Medina <medinatorresomaryesid@gmail.com>
2022-10-01 15:45:02 -0500

    Modificación del archivo app.vue

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         <button @click="loadCliente">Cliente</button>
         <button @click="logout">Cerrar Sesión</button>
       </nav>
-      <nav v-else>
+      <nav v-else> 
         <button @click="loadLogin">Iniciar Sesión</button>
         <button @click="loadSignUp">Registrarse</button>
       </nav>
@@ -24,126 +24,125 @@
   </div>
 </template>
 <script>
-export default {
-  name: 'App',
-  data: function () {
-    return {
-      is_auth: false,
-    }
-  },
-  methods: {
-    verifyAuth: function () {
-      this.is_auth = localStorage.getItem("isAuth") || false;
+  export default {
+    name: 'App',
+    data: function(){
+      return {
+        is_auth: false,
+      }
+    },
+    methods: {
+      verifyAuth: function() {
+        this.is_auth = localStorage.getItem("isAuth") || false;
 
-      if (this.is_auth)
-        this.$router.push({ name: "home" })
-      else
-        this.$router.push({ name: "login" })
-    },
-    loadCliente: function () {
-      this.$router.push({ name: "cliente" })
-    },
-    loadHome: function () {
-      this.$router.push({ name: "home" })
-    },
-    loadLogin: function () {
-      this.$router.push({ name: "login" })
-    },
-    loadSignUp: function () {
-      this.$router.push({ name: "signup" })
-    },
-    completedLogin: function (data) {
-      localStorage.setItem("isAuth", true);
-      localStorage.setItem("username", data.username);
-      localStorage.setItem("token_access", data.token_access);
-      localStorage.setItem("token_refresh", data.token_refresh);
-      alert("Autenticación Exitosa");
-      this.verifyAuth();
-    },
-    completedSignUp: function (data) {
-      alert("Registro Exitoso");
-      this.completedLogIn(data);
-    },
-    logout: function () {
-      localStorage.clear();
-      alert("Sesión Cerrada");
-      this.verifyAuth();
+        if(this.is_auth)
+          this.$router.push({name: "home"})
+        else
+          this.$router.push({name: "login"})
+      },
+      loadCliente: function(){
+        this.$router.push({name: "cliente"})
+      },
+      loadHome: function(){
+        this.$router.push({name: "home"})
+      },
+      loadLogin: function(){
+        this.$router.push({name: "login"})
+      },
+      loadSignUp: function(){
+        this.$router.push({name: "signup"})
+      },
+      completedLogin: function(data){
+        localStorage.setItem("isAuth",true);
+        localStorage.setItem("username",data.username);
+        localStorage.setItem("token_access",data.token_access);
+        localStorage.setItem("token_refresh",data.token_refresh);
+        alert("Autenticación Exitosa");
+        this.verifyAuth();
+      },
+      completedSignUp: function(data){
+        alert("Registro Exitoso");
+        this.completedLogIn(data);    
+      },
+      logout: function(){
+        localStorage.clear();
+        alert("Sesión Cerrada");
+        this.verifyAuth();
+      }
     }
   }
-}
 </script>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto');
+  *{
+    margin: 0;
+    padding: 0;
+  }
 
-* {
-  margin: 0;
-  padding: 0;
-}
+  .body{
+    font-family: 'Roboto','sans-serif';
+  }
 
-.body {
-  font-family: 'Roboto', 'sans-serif';
-}
+  .header{
+    width: 100%;
+    height: 5vh;
+    min-height: 90px;
+    background-color: #0c181c;
+    color: rgb(195, 167, 164);
+    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
-.header {
-  width: 100%;
-  height: 5vh;
-  min-height: 90px;
-  background-color: #0c181c;
-  color: rgb(195, 167, 164);
+  .header h1{
+    width: 20%;
+    text-align: center;
+  }
 
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+  .header nav{
+    height: 100%;
+    width: 20%;
+    
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
 
-.header h1 {
-  width: 20%;
-  text-align: center;
-}
+  .header nav button{
+    color: rgb(195, 167, 164);
+    background-color: #0c181c;
+    border: 1px solid rgb(195, 167, 164);
 
-.header nav {
-  height: 100%;
-  width: 20%;
+    border-radius: 5px;
+    padding: 10px 20px;
+  }
 
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-}
+  .header nav button:hover{
+    background-color: #E5E7E9;
+    color: #0c181c;
+  }
 
-.header nav button {
-  color: rgb(195, 167, 164);
-  background-color: #0c181c;
-  border: 1px solid rgb(195, 167, 164);
+  .main-component{
+    height: 10vh;
+    min-height: 100vh;
+    background-color: #FDFEFE;
+  }
 
-  border-radius: 5px;
-  padding: 10px 20px;
-}
+  .footer{
+    width: 100%;
+    height: 10vh;
+    min-height: 80px;
+    background-color: #0c181c;
+    color: rgb(195, 167, 164);
+  }
 
-.header nav button:hover {
-  background-color: #E5E7E9;
-  color: #0c181c;
-}
+  .footer h2{
+    width: 100%;
+    height: 100%;
 
-.main-component {
-  height: 10vh;
-  min-height: 100vh;
-  background-color: #FDFEFE;
-}
-
-.footer {
-  width: 100%;
-  height: 10vh;
-  min-height: 80px;
-  background-color: #0c181c;
-  color: rgb(195, 167, 164);
-}
-
-.footer h2 {
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
